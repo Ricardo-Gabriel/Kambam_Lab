654091eed00493a21bea06fbbb7f02a3d62810a4
Ricardo-Gabriel <69832153+Ricardo-Gabriel@users.noreply.github.com>
2023-03-15 14:03:54 -0300

    kambam pronto (sem som)

--- kambam.js
+++ kambam.js
@@ -1,7 +1,8 @@
 // Get the elements that will be dragged
-const draggables = document.querySelectorAll(".draggable");
+const draggables = document.querySelectorAll(".card");
 // Get the dropzone
-const dropzone = document.querySelector(".dropzone");
+const drop1 = document.querySelector(".drop1");
+
 
 // Loop through the draggable elements
 draggables.forEach((draggable) => {
@@ -15,27 +16,83 @@ draggables.forEach((draggable) => {
     draggable.classList.remove("dragging");
   });
 });
-
+//drop zone 1 init
 // Add event listeners for when the element is being dragged over the dropzone
-dropzone.addEventListener("dragover", (e) => {
+drop1.addEventListener("dragover", (e) => {
   // Prevent the default behavior of not allowing elements to be dropped
   e.preventDefault();
   // Add a class to the dropzone to style it
-  dropzone.classList.add("dragover");
+  drop1.classList.add("dragover");
 });
 
 // Add event listeners for when the element is no longer being dragged over the dropzone
-dropzone.addEventListener("dragleave", () => {
+drop1.addEventListener("dragleave", () => {
   // Remove the class from the dropzone
-  dropzone.classList.remove("dragover");
+  drop1.classList.remove("dragover");
 });
 
 // Add event listeners for when the element is dropped onto the dropzone
-dropzone.addEventListener("drop", () => {
+drop1.addEventListener("drop", () => {
   // Get the element that was dropped
   const draggable = document.querySelector(".dragging");
   // Append the element to the dropzone
-  dropzone.appendChild(draggable);
+  drop1.appendChild(draggable);
   // Remove the class from the dropzone
-  dropzone.classList.remove("dragover");
+  drop1.classList.remove("dragover");
 });
+//drop zone 1 end
+
+//drop zone 2 init 
+const drop2 = document.querySelector(".drop2");
+
+
+drop2.addEventListener("dragover", (e) => {
+    // Prevent the default behavior of not allowing elements to be dropped
+    e.preventDefault();
+    // Add a class to the dropzone to style it
+    drop2.classList.add("dragover");
+  });
+  
+  // Add event listeners for when the element is no longer being dragged over the dropzone
+  drop2.addEventListener("dragleave", () => {
+    // Remove the class from the dropzone
+    drop2.classList.remove("dragover");
+  });
+  
+  // Add event listeners for when the element is dropped onto the dropzone
+  drop2.addEventListener("drop", () => {
+    // Get the element that was dropped
+    const draggable = document.querySelector(".dragging");
+    // Append the element to the dropzone
+    drop2.appendChild(draggable);
+    // Remove the class from the dropzone
+    drop2.classList.remove("dragover");
+  });
+// drop zone 2 end
+
+// drop zone 3 init
+const drop3 = document.querySelector(".drop3");
+
+
+drop3.addEventListener("dragover", (e) => {
+    // Prevent the default behavior of not allowing elements to be dropped
+    e.preventDefault();
+    // Add a class to the dropzone to style it
+    drop2.classList.add("dragover");
+  });
+  
+  // Add event listeners for when the element is no longer being dragged over the dropzone
+  drop3.addEventListener("dragleave", () => {
+    // Remove the class from the dropzone
+    drop3.classList.remove("dragover");
+  });
+  
+  // Add event listeners for when the element is dropped onto the dropzone
+  drop3.addEventListener("drop", () => {
+    // Get the element that was dropped
+    const draggable = document.querySelector(".dragging");
+    // Append the element to the dropzone
+    drop3.appendChild(draggable);
+    // Remove the class from the dropzone
+    drop3.classList.remove("dragover");
+  });
